a5b5f7a6999f5c3184f0f35c70f6196bc6006f4a
Samet ÇEVİK <samtcevik@gmail.com>
2022-02-11 03:25:55 +0300

    SRP, OCP

---
+++ Solid.OpenClosed/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//The Scenario is not proper to the open-closed principle.
+
+public class FullTime {
+    public void CalculatorSalary() {
+        Console.WriteLine("FullTime");
+    }
+}
+
+public class PartTime {
+    public void CalculatorSalary() {
+        Console.WriteLine("PartTime");
+    }
+}
+
+public class Calculator {
+    public void CalculatorSalary(string employeeType) {
+        if(employeeType == null) {
+            new ArgumentNullException("You have to set employee type");
+        } else if(employeeType == "FullTime") {
+            new FullTime().CalculatorSalary();
+        } else {
+            new PartTime().CalculatorSalary();
+        }
+    }
+}
+
+//Let's say we decided to hire support staff on special occasions.
+//We need to change the salary calculation method for each new class we add.
+//This is not a desired situation. It causes too many conditional cases,
+//and while it may seem harmless at first, the method becomes more complex
+//as the number of classes increases.
+public class Supporter {
+    public void CalculatorSalary() {
+        Console.WriteLine("Supporter");
+    }
+}
+
+public class Calculator2 {
+    public void CalculatorSalary(string employeeType) {
+        if(employeeType == null) {
+            new ArgumentNullException("You have to set employee type");
+        } else if(employeeType == "FullTime") {
+            new FullTime().CalculatorSalary();
+        } else if(employeeType == "Supporter") {
+            new Supporter().CalculatorSalary();
+        } else {
+            new PartTime().CalculatorSalary();
+        }
+    }
+}
+
--- Solid.SingleResponsibility/Program.cs
+++ Solid.SingleResponsibility/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+//The Class is not proper to the Single Responsibility Principle.
+public class Customer {
+    List<Customer> list = new List<Customer>() {
+        new Customer() {Name = "Samet", Id = 1},
+        new Customer() {Name ="Esma", Id=2}
+    };
+    public string Name { get; set; }
+    public int Id { get; set; }
+
+    public bool ValidateName(string name) => !string.IsNullOrEmpty(name);
+
+    public string GetName(int id) => list.Find(x => x.Id == id).Name;
+}
+
+//Separated Classes according to responsibilities.
+public class Customer2 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class CustomerManager {
+
+    List<Customer> list = new List<Customer>() {
+        new Customer() {Name = "Samet", Id = 1},
+        new Customer() {Name ="Esma", Id=2}
+    };
+    public string GetName(int id) => list.Find(x => x.Id == id).Name;
+}
+
+public class CustomerValidator {
+    public bool ValidateName(string name) => !string.IsNullOrEmpty(name);
+}
